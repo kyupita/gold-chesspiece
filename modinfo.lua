84b248d9954c50b216818d56337e4b597911f048
José Fernando Domínguez Chávez <107364728+JFDomChav@users.noreply.github.com>
2025-11-02 14:51:26 -0600

    modmain and modinfo

---
+++ modinfo.lua
@@ -0,0 +1,13 @@
+name = "Golden Sculptures"
+description = "Adds golden variants of the 44 chesspieces"
+author = "Kyupita & Chestworth"
+version = "1.0.0"
+
+api_version = 10
+dst_compatible = true
+
+client_only_mod = false
+all_clients_require_mod = true
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
---
+++ modmain.lua
@@ -0,0 +1,365 @@
+PrefabFiles = {
+	"chesspiece_anchor_gold",
+	"chesspiece_antlion_gold",
+	"chesspiece_bearger_gold",
+	"chesspiece_bearger_mutated_gold",
+	"chesspiece_beefalo_gold",
+	"chesspiece_beequeen_gold",
+	"chesspiece_bishop_gold",
+	"chesspiece_butterfly_gold",
+	"chesspiece_carrat_gold",
+	"chesspiece_catcoon_gold",
+	"chesspiece_clayhound_gold",
+	"chesspiece_claywarg_gold",
+	"chesspiece_crabking_gold",
+	"chesspiece_daywalker_gold",
+	"chesspiece_daywalker2_gold",
+	"chesspiece_deerclops_gold",
+	"chesspiece_deerclops_mutated_gold",
+	"chesspiece_dragonfly_gold",
+	"chesspiece_eyeofterror_gold",
+	"chesspiece_formal_gold",
+	"chesspiece_guardianphase3_gold",
+	"chesspiece_hornucopia_gold",
+	"chesspiece_kitcoon_gold",
+	"chesspiece_klaus_gold",
+	"chesspiece_knight_gold",
+	"chesspiece_malbatross_gold",
+	"chesspiece_manrabbit_gold",
+	"chesspiece_minotaur_gold",
+	"chesspiece_moon_gold",
+	"chesspiece_moosegoose_gold",
+	"chesspiece_muse_gold",
+	"chesspiece_pawn_gold",
+	"chesspiece_pipe_gold",
+	"chesspiece_rook_gold",
+	"chesspiece_sharkboi_gold",
+	"chesspiece_stalker_gold",
+	"chesspiece_toadstool_gold",
+	"chesspiece_twinsofterror_gold",
+	"chesspiece_wagboss_lunar_gold",
+	"chesspiece_wagboss_robot_gold",
+	"chesspiece_warg_mutated_gold",
+	"chesspiece_wormboss_gold",
+	"chesspiece_yotd_gold",
+	"chesspiece_yots_gold",
+}
+
+--[[Assets = {
+	Asset("ANIM", "anim/chesspiece_anchor_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_anchor_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_anchor_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_antlion_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_antlion_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_antlion_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_bearger_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_bearger_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_bearger_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_bearger_mutated_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_bearger_mutated_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_bearger_mutated_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_beefalo_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_beefalo_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_beefalo_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_beequeen_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_beequeen_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_beequeen_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_bishop_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_bishop_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_bishop_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_butterfly_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_butterfly_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_butterfly_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_carrat_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_carrat_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_carrat_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_catcoon_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_catcoon_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_catcoon_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_clayhound_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_clayhound_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_clayhound_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_claywarg_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_claywarg_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_claywarg_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_crabking_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_crabking_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_crabking_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_daywalker_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_daywalker_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_daywalker_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_daywalker2_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_daywalker2_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_daywalker2_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_deerclops_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_deerclops_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_deerclops_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_deerclops_mutated_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_deerclops_mutated_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_deerclops_mutated_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_dragonfly_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_dragonfly_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_dragonfly_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_eyeofterror_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_eyeofterror_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_eyeofterror_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_formal_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_formal_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_formal_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_guardianphase3_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_guardianphase3_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_guardianphase3_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_hornucopia_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_hornucopia_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_hornucopia_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_kitcoon_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_kitcoon_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_kitcoon_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_klaus_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_klaus_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_klaus_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_knight_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_knight_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_knight_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_malbatross_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_malbatross_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_malbatross_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_manrabbit_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_manrabbit_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_manrabbit_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_minotaur_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_minotaur_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_minotaur_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_moon_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_moon_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_moon_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_moosegoose_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_moosegoose_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_moosegoose_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_muse_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_muse_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_muse_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_pawn_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_pawn_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_pawn_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_pipe_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_pipe_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_pipe_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_rook_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_rook_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_rook_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_sharkboi_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_sharkboi_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_sharkboi_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_stalker_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_stalker_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_stalker_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_toadstool_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_toadstool_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_toadstool_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_twinsofterror_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_twinsofterror_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_twinsofterror_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_wagboss_lunar_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_wagboss_lunar_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_wagboss_lunar_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_wagboss_robot_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_wagboss_robot_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_wagboss_robot_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_warg_mutated_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_warg_mutated_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_warg_mutated_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_wormboss_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_wormboss_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_wormboss_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_yotd_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_yotd_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_yotd_gold.tex"),
+	
+	Asset("ANIM", "anim/chesspiece_yots_gold.zip"),
+	Asset("ATLAS", "images/chesspiece_yots_gold.xml"),
+    Asset("IMAGE", "images/chesspiece_yots_gold.tex"),
+}]]
+
+RECIPETABS = GLOBAL.RECIPETABS
+Recipe = GLOBAL.Recipe
+Ingredient = GLOBAL.Ingredient
+TECH = GLOBAL.TECH
+AllRecipes = GLOBAL.AllRecipes
+STRINGS = GLOBAL.STRINGS
+local Materials = {Ingredient("rocks", 2)}
+STech = TECH.SCULPTING
+
+AddRecipe2("chesspiece_anchor_gold", Materials, STech, {atlas = "images/chesspiece_anchor_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_ANCHOR_GOLD = "Ground your base with a stone anchor."
+
+AddRecipe2("chesspiece_antlion_gold", Materials, STech, {atlas = "images/chesspiece_antlion_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_ANTLION_GOLD = "Ain't this Antlion beautiful?"
+
+AddRecipe2("chesspiece_bearger_gold", Materials, STech, {atlas = "images/chesspiece_bearger_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_BEARGER_GOLD = "A stone reminder of a grizzly autumn."
+
+AddRecipe2("chesspiece_bearger_mutated_gold", Materials, STech, {atlas = "images/chesspiece_bearger_mutated_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_BEARGER_MUTATED_GOLD = "A soul-bearing rendition of a ferocious favorite."
+
+AddRecipe2("chesspiece_beefalo_gold", Materials, STech, {atlas = "images/chesspiece_beefalo_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_BEEFALO_GOLD = "A winsome sculpture of a champion."
+
+AddRecipe2("chesspiece_beequeen_gold", Materials, STech, {atlas = "images/chesspiece_beequeen_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_BEEQUEEN_GOLD = "More regal than real."
+
+AddRecipe2("chesspiece_bishop_gold", Materials, STech, {atlas = "images/chesspiece_bishop_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_BISHOP_GOLD = "A chesspiece with a checkered past."
+
+AddRecipe2("chesspiece_butterfly_gold", Materials, STech, {atlas = "images/chesspiece_butterfly_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_BUTTERFLY_GOLD = "This sculpture's a moth-have."
+
+AddRecipe2("chesspiece_carrat_gold", Materials, STech, {atlas = "images/chesspiece_carrat_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_CARRAT_GOLD = "A winsome sculpture of a champion."
+
+AddRecipe2("chesspiece_catcoon_gold", Materials, STech, {atlas = "images/chesspiece_catcoon_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_CATCOON_GOLD = "A curious statue for a curious creature."
+
+AddRecipe2("chesspiece_clayhound_gold", Materials, STech, {atlas = "images/chesspiece_clayhound_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_CLAYHOUND_GOLD = "It ain't nothing but a clay hound dog."
+
+AddRecipe2("chesspiece_claywarg_gold", Materials, STech, {atlas = "images/chesspiece_claywarg_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_CLAYWARG_GOLD = "Sculpt a fearsome Clay Varg."
+
+AddRecipe2("chesspiece_crabking_gold", Materials, STech, {atlas = "images/chesspiece_crabking_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_CRABKING_GOLD = "It looks even crabbier than you remember it."
+
+AddRecipe2("chesspiece_daywalker_gold", Materials, STech, {atlas = "images/chesspiece_daywalker_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_DAYWALKER_GOLD = "A stone likeness of a pig-headed prisoner."
+
+AddRecipe2("chesspiece_daywalker2_gold", Materials, STech, {atlas = "images/chesspiece_daywalker2_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_DAYWALKER2_GOLD = "He sure was a scrappy fellow."
+
+AddRecipe2("chesspiece_deerclops_gold", Materials, STech, {atlas = "images/chesspiece_deerclops_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_DEERCLOPS_GOLD = "Sculpt yourself an eyeball monster."
+
+AddRecipe2("chesspiece_deerclops_mutated_gold", Materials, STech, {atlas = "images/chesspiece_deerclops_mutated_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_DEERCLOPS_MUTATED_GOLD = "A truly eye-catching sculpture."
+
+AddRecipe2("chesspiece_dragonfly_gold", Materials, STech, {atlas = "images/chesspiece_dragonfly_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_DRAGONFLY_GOLD = "The ultimate trophy for the ultimate kill."
+
+AddRecipe2("chesspiece_eyeofterror_gold", Materials, STech, {atlas = "images/chesspiece_eyeofterror_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_EYEOFTERROR_GOLD = "Its stony gaze reminds you of an evil presence."
+
+AddRecipe2("chesspiece_formal_gold", Materials, STech, {atlas = "images/chesspiece_formal_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_FORMAL_GOLD = "Behind every queen is a dapper king."
+
+AddRecipe2("chesspiece_guardianphase3_gold", Materials, STech, {atlas = "images/chesspiece_guardianphase3_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_GUARDIANPHASE3_GOLD = "Its design radiates an otherworldly strangeness."
+
+AddRecipe2("chesspiece_hornucopia_gold", Materials, STech, {atlas = "images/chesspiece_hornucopia_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_HORNUCOPIA_GOLD = "A sculpture for a starving artist."
+
+AddRecipe2("chesspiece_kitcoon_gold", Materials, STech, {atlas = "images/chesspiece_kitcoon_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_KITCOON_GOLD = "One's true wealth is measured by their friends."
+
+AddRecipe2("chesspiece_klaus_gold", Materials, STech, {atlas = "images/chesspiece_klaus_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_KLAUS_GOLD = "Klaus to perfection."
+
+AddRecipe2("chesspiece_knight_gold", Materials, STech, {atlas = "images/chesspiece_knight_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_KNIGHT_GOLD = "Shining armor not included."
+
+AddRecipe2("chesspiece_malbatross_gold", Materials, STech, {atlas = "images/chesspiece_malbatross_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_MALBATROSS_GOLD = "A sculpted scourge of the seas."
+
+AddRecipe2("chesspiece_manrabbit_gold", Materials, STech, {atlas = "images/chesspiece_manrabbit_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_MANRABBIT_GOLD = "A fluffy friend, made of rock."
+
+AddRecipe2("chesspiece_minotaur_gold", Materials, STech, {atlas = "images/chesspiece_minotaur_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_MINOTAUR_GOLD = "This guardian sits in stony silence."
+
+AddRecipe2("chesspiece_moon_gold", Materials, STech, {atlas = "images/chesspiece_moon_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_MOON_GOLD = "It's a selene figurine!"
+
+AddRecipe2("chesspiece_moosegoose_gold", Materials, STech, {atlas = "images/chesspiece_moosegoose_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_MOOSEGOOSE_GOLD = "Two for the price of one."
+
+AddRecipe2("chesspiece_muse_gold", Materials, STech, {atlas = "images/chesspiece_muse_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_MUSE_GOLD = "The most powerful piece on the board."
+
+AddRecipe2("chesspiece_pawn_gold", Materials, STech, {atlas = "images/chesspiece_pawn_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_PAWN_GOLD = "It takes a village to raise an army."
+
+AddRecipe2("chesspiece_pipe_gold", Materials, STech, {atlas = "images/chesspiece_pipe_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_PIPE_GOLD = "Stick this on your wheel and sculpt it."
+
+AddRecipe2("chesspiece_rook_gold", Materials, STech, {atlas = "images/chesspiece_rook_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_ROOK_GOLD = "Next best thing to an actual castle."
+
+AddRecipe2("chesspiece_sharkboi_gold", Materials, STech, {atlas = "images/chesspiece_sharkboi_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_SHARKBOI_GOLD = "An homage to a cold-blooded bully."
+
+AddRecipe2("chesspiece_stalker_gold", Materials, STech, {atlas = "images/chesspiece_stalker_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_STALKER_GOLD = "A de-animated reanimated skeleton."
+
+AddRecipe2("chesspiece_toadstool_gold", Materials, STech, {atlas = "images/chesspiece_toadstool_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_TOADSTOOL_GOLD = "A toad-ally authentic reproduction of the original."
+
+AddRecipe2("chesspiece_twinsofterror_gold", Materials, STech, {atlas = "images/chesspiece_twinsofterror_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_TWINSOFTERROR_GOLD = "Its stony gaze reminds you of an evil presence."
+
+AddRecipe2("chesspiece_wagboss_lunar_gold", Materials, STech, {atlas = "images/chesspiece_wagboss_lunar_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_WAGBOSS_LUNAR_GOLD = "The abomination."
+
+AddRecipe2("chesspiece_wagboss_robot_gold", Materials, STech, {atlas = "images/chesspiece_wagboss_robot_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_WAGBOSS_ROBOT_GOLD = "The automation."
+
+AddRecipe2("chesspiece_warg_mutated_gold", Materials, STech, {atlas = "images/chesspiece_warg_mutated_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_WARG_MUTATED_GOLD = "A soul-bearing rendition of a ferocious favorite."
+
+AddRecipe2("chesspiece_wormboss_gold", Materials, STech, {atlas = "images/chesspiece_wormboss_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_WORMBOSS_GOLD = "Brings back memories of being digested."
+
+AddRecipe2("chesspiece_yotd_gold", Materials, STech, {atlas = "images/chesspiece_yotd_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_YOTD_GOLD = "A tribute to getting things off to a fiery start."
+
+AddRecipe2("chesspiece_yots_gold", Materials, STech, {atlas = "images/chesspiece_yots_gold.xml"})
+	STRINGS.RECIPE_DESC.CHESSPIECE_YOTS_GOLD = "A reminder of a lucky worm encounter."
